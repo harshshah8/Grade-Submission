131fc6af80c05e25ffc7ae7b6ac6c78a4be664ba
Harsh Shah <harsh@Harshs-MacBook-Air.local>
2022-11-22 22:49:00 +0530

    Model and View

--- src/main/java/com/ltp/gradesubmission/Grade.java
+++ src/main/java/com/ltp/gradesubmission/Grade.java
@@ -1,17 +1,21 @@
 package com.ltp.gradesubmission;
 
+import java.util.UUID;
+
 public class Grade {
     private String name;
     private String subject;
     private String score;
+    private String id;
 
-    public Grade(String name, String subject, String score) {
-        this.name = name;
-        this.subject = subject;
-        this.score = score;
-    }
+    // public Grade(String name, String subject, String score) {
+    //     this.name = name;
+    //     this.subject = subject;
+    //     this.score = score;
+    // }
 
     public Grade() {
+        this.id = UUID.randomUUID().toString();
     }
 
     public String getName() {
@@ -37,5 +41,15 @@ public class Grade {
     public void setScore(String score) {
         this.score = score;
     }
+
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
      
 }
--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.naming.LinkLoopException;
+
+import org.apache.tomcat.util.bcel.Const;
+import org.springframework.boot.devtools.tunnel.payload.HttpTunnelPayload;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,32 +26,32 @@ public class GradeController {
     }
 
     @GetMapping("/")
-    public String gradeForm(Model model,@RequestParam(required = false) String name)
+    public String gradeForm(Model model,@RequestParam(required = false) String id)
     {
         
-         model.addAttribute("grade", getGradeIndex(name)==-1000?  new Grade():studenGrades.get(getGradeIndex(name)));
+         model.addAttribute("grade", getGradeIndex(id)==Constant.NOT_FOUND?  new Grade():studenGrades.get(getGradeIndex(id)));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitForm(Grade grade)
     {
-        int index = getGradeIndex(grade.getName());
-        if(index==-1000)
+        int index = getGradeIndex(grade.getId());
+        if(index==Constant.NOT_FOUND)
             studenGrades.add(grade);
         else
             studenGrades.set(index, grade);
         return "redirect:/grades";
     }
 
-    public Integer getGradeIndex(String names)
+    public Integer getGradeIndex(String id)
     {   
         for(int i=0;i<studenGrades.size();i++)
         {
-            if(studenGrades.get(i).getName().equals(names))
+            if(studenGrades.get(i).getId().equals(id))
                 return i;
         }
-        return -1000;
+        return Constant.NOT_FOUND;
 
     }
     
