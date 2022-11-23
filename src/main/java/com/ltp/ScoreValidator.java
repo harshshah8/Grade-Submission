bdbf761d6d1834dd7e37e049ce4ea007c934ce4d
Harsh Shah <harsh@Harshs-MacBook-Air.local>
2022-11-23 11:16:36 +0530

    added custom validation

---
+++ src/main/java/com/ltp/ScoreValidator.java
@@ -0,0 +1,30 @@
+package com.ltp;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.ltp.gradesubmission.Score; 
+
+public class ScoreValidator implements ConstraintValidator<Score , String>{
+    
+    List<String> scores = Arrays.asList(
+        "A+", "A", "A-",
+        "B+", "B", "B-",
+        "C+", "C", "C-",
+        "D+", "D", "D-",
+        "F"
+    );
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        for(String it:scores)
+        {
+            if(value.equals(it))
+                return true;
+        }
+ 
+        return false;
+    }
+}
