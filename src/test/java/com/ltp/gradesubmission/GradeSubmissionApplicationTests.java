f0c840c366c4759ab44cd8540a3c473af4c66017
Harsh Shah <harsh@Harshs-MacBook-Air.local>
2022-12-15 16:09:13 +0530

    integration testing - 2

--- src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -20,5 +24,54 @@ class GradeSubmissionApplicationTests {
 	void contextLoads() {
 		assertNotNull(mockMvc);
 	}
+
+	@Test
+	public void testShowGradeForm () throws Exception
+	{
+		 RequestBuilder request = MockMvcRequestBuilders.get("/?id=123");
+		  mockMvc.perform(request)
+		  	.andExpect(status().is2xxSuccessful())
+			.andExpect(view().name("form"))
+			.andExpect(model().attributeExists("grade")); 
+ 
+	}
+
+	@Test
+	public void testSuccessfulSubmission() throws Exception
+	{
+		RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
+			.param("name", "Harsh")
+			.param("subject","English")
+			.param("score","A-");
+
+		mockMvc.perform(request)
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("/grades"));
+
+	}
+
+	@Test
+	public void testUnsuccessfulSubmission() throws Exception
+	{
+		RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
+		.param("name", " ")
+		.param("subject"," ")
+		.param("score"," ");
+
+		mockMvc.perform(request)
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(view().name("form"));
+
+	} 
+
+	@Test
+	public void testGetGrades() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/grades");
+		mockMvc.perform(request)
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(view().name("grades"))
+			.andExpect(model().attributeExists("grade"));
+	}
+	
 
 }
